8b31d99eac0b3eb25b26e79ffccc5e8f75c6e35f
Rodrigo Bersa <rodrigo.bersa@gmail.com>
2024-05-17 21:29:00 -0400

    Initial commit

---
+++ bootstrap/terraform/apps.tf
@@ -0,0 +1,140 @@
+resource "kubernetes_deployment_v1" "collector" {
+  metadata {
+    name      = "collector"
+    namespace = "default"
+    labels = {
+      app = "collector"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "collector"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "collector"
+        }
+      }
+
+      spec {
+        container {
+          image = "public.ecr.aws/aws-cli/aws-cli:2.15.53"
+          name  = "collector"
+          args  = ["s3", "ls"]
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+        }
+        service_account_name = "collector-sa"
+      }
+    }
+  }
+  depends_on = [ module.eks ]
+}
+
+resource "kubernetes_service_account_v1" "collector" {
+  metadata {
+    name      = "collector-sa"
+    namespace = "default"
+  }
+  depends_on = [ module.eks ]
+}
+
+resource "kubernetes_deployment_v1" "threat" {
+  metadata {
+    name      = "threat"
+    namespace = "kube-system"
+    labels = {
+      app = "threat"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "threat"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "threat"
+        }
+      }
+
+      spec {
+        container {
+          image   = "public.ecr.aws/lts/ubuntu:latest"
+          name    = "threat"
+          command = ["sleep"]
+          args    = ["infinity"]
+
+          resources {
+            limits = {
+              cpu    = "0.5"
+              memory = "512Mi"
+            }
+            requests = {
+              cpu    = "250m"
+              memory = "50Mi"
+            }
+          }
+          port {
+            container_port = 22
+          }
+          security_context {
+            privileged = true
+          }
+          volume_mount {
+            name       = "host-etc"
+            mount_path = "/host-etc"
+          }
+        }
+        volume {
+          name = "host-etc"
+          host_path {
+            path = "/etc"
+          }
+        }
+        service_account_name = "default"
+      }
+    }
+  }
+  depends_on = [ module.eks ]
+}
+
+resource "kubernetes_role_binding_v1" "threat" {
+  metadata {
+    name      = "threat"
+    namespace = "kube-system"
+  }
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+  subject {
+    kind      = "ServiceAccount"
+    name      = "default"
+    namespace = "kube-system"
+  }
+  depends_on = [ module.eks ]
+}
---
+++ bootstrap/terraform/eks.tf
@@ -0,0 +1,58 @@
+################################################################################
+# EKS Cluster
+################################################################################
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.5"
+
+  cluster_name                   = local.name
+  cluster_version                = "1.29"
+  cluster_endpoint_public_access = true
+
+  cluster_enabled_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent    = true
+      before_compute = true
+    }
+    eks-pod-identity-agent = {
+      most_recent = true
+    }
+    # aws-guardduty-agent = {
+    #   most_recent = true
+    # }
+
+  }
+  # Give the Terraform identity admin access to the cluster
+  # which will allow resources to be deployed into the cluster
+  enable_cluster_creator_admin_permissions = true
+  authentication_mode                      = "API"
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["m5.large", "m5a.large"]
+      iam_role_attach_cni_policy = true
+
+      min_size     = 1
+      max_size     = 5
+      desired_size = 3
+
+      ebs_optimized     = true
+      enable_monitoring = true
+    }
+  }
+
+  tags = merge(local.tags, {
+    "karpenter.sh/discovery" = local.name
+  })
+}
