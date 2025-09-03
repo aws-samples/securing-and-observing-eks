4b9af1702a56514339637ada968f50e0f0ffbf3d
Rodrigo Bersa <rodrigo.bersa@gmail.com>
2025-09-03 15:15:20 -0400

    Converting EKS cluster to Auto Mode

--- bootstrap/terraform/variables.tf
+++ bootstrap/terraform/variables.tf
@@ -1,3 +1,9 @@
 variable "name" {
   description = "Name to be used on all resources."
 }
+
+variable "version" {
+  description = "Amazon EKS Auto Mode Cluster Version"
+  type        = string
+  default     = "1.33"
+}
