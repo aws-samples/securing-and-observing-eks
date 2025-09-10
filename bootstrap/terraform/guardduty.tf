fbddcd7114a7e5f79f2c39505eb3f44c9dff7e7a
Rodrigo Bersa <rodrigo.bersa@gmail.com>
2025-09-10 12:09:10 -0400

    Commenting GuardDuty for now

--- bootstrap/terraform/guardduty.tf
+++ bootstrap/terraform/guardduty.tf
@@ -25,15 +25,15 @@ resource "aws_security_group" "guardduty_endpoint" {
   }
 }
 
-module "standalone_guardduty" {
-  source  = "aws-ia/guardduty/aws"
-  version = "0.0.2"
+# module "standalone_guardduty" {
+#   source  = "aws-ia/guardduty/aws"
+#   version = "~> 0.1"
 
-  enable_guardduty             = true
-  enable_s3_protection         = true
-  enable_kubernetes_protection = true
-  enable_malware_protection    = true
-  enable_snapshot_retention    = true
-  finding_publishing_frequency = "FIFTEEN_MINUTES"
-  tags                         = {}
-}
+#   enable_guardduty              = true
+#   enable_kubernetes_protection  = true
+#   enable_eks_runtime_monitoring = true
+#   enable_ec2_runtime_monitoring = true
+#   enable_malware_protection     = true
+#   finding_publishing_frequency  = "FIFTEEN_MINUTES"
+#   tags                          = {}
+# }
